f3b347abb5532a9ce673ffaf9375cf03276287cd
Fernando Ramirez <99226507+fgr101@users.noreply.github.com>
2026-03-04 00:59:36 -0300

    Add files via upload

--- Limpia.c
+++ Limpia.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> //*This is the library to work with strings.
+#include <time.h>
 
 #ifdef _WIN32
 
@@ -24,10 +25,17 @@ void ServicesDeactivate();
 void RunCleanManager();
 void AboutLimpia();
 void AutoCleaning();
+void NewEntryFile();
 
 char input;
 int opcion;
 int SwitchAuto;
+
+char fecha[50];
+int ActionNumber;
+char ActionText[300];
+
+FILE *fptr; // Declare the file pointer globally
 
 int main() {
 	
@@ -47,61 +55,71 @@ int main() {
     switch (opcion) {
 		
 		case 1:
-		
+			
+			ActionNumber = opcion;
 			UpdateWinget();
 			goto ShowMenuINI;
 			break;
 			
 		case 2:
-		
+			
+			ActionNumber = opcion;
 			DelTempFiles();
 			goto ShowMenuINI;	
 			break;
 			
 		case 3:
-		
+			
+			ActionNumber = opcion;
 			KillTempFolder();
 			goto ShowMenuINI;	
 			break;			
 				
 		case 4:
 		
+			ActionNumber = opcion;
 			DelRecycleBin();
 			goto ShowMenuINI;
 			break;
 
 		case 5:
 			
+			ActionNumber = opcion;
 			BadServices();
 			goto ShowMenuINI;
 			break;
 			
 		case 6:
 		
+			ActionNumber = opcion;
 			FlushDNS();
 			goto ShowMenuINI;
 			break;
 			
 		case 7:
 		
+			ActionNumber = opcion;
 			CleanChrome();
 			goto ShowMenuINI;
 			break;
 
 		case 8:
-		
+			
+			ActionNumber = opcion;	
 			CleanStreamio();
 			goto ShowMenuINI;
 			break;
 			
 		case 9:
-
+			
+			ActionNumber = opcion;
 			MenuServicios();
 			goto ShowMenuINI;
 			break;
 			
 		case 10:
-		
+			
+			ActionNumber = opcion;
 			RunCleanManager();
 			WaitKey();
 			opcion = 0;
@@ -117,6 +135,7 @@ int main() {
 		case 12:
 		
 			SwitchAuto = 1;
+			ActionNumber = opcion;
 			AutoCleaning();
 			goto ShowMenuINI;
 			break;
@@ -282,6 +301,7 @@ void DelRecycleBin() {
 	printf("\nLimpiando la papelera de reciclaje...\n");
     system("rd /s /q C:\\$Recycle.Bin");
     printf("\nPapelera de reciclaje vacia.\n");
+    NewEntryFile();
     WaitKey();
     return;
 	
@@ -294,6 +314,7 @@ void DelTempFiles() {
 	printf("\nBorrando archivos temporales...");
 	system("del /f /s /q %TEMP%\\*");
 	printf("\nArchivos temporales eliminados!");
+	NewEntryFile();
 	WaitKey();
 	return;
 	
@@ -307,6 +328,7 @@ void KillTempFolder() {
     system("rd /s /q %temp%");
     system("md %temp%");
     printf("\nCarpeta temporal recreada.\n");
+    NewEntryFile();
     WaitKey();
     return;
 }
@@ -327,11 +349,13 @@ void UpdateWinget() {
 	
 	if (opcion != 197) {
 		
-		printf ("\n\nAccion cancelada...\n");		
+		printf ("\n\nAccion cancelada...\n");
+	    NewEntryFile();		
 		return;
 		
 	} else { 
 		
+	    NewEntryFile();
 		printf ("\n\nAccion confirmada...\n");
 		
 	}
@@ -340,6 +364,7 @@ void UpdateWinget() {
     system("winget upgrade --include-unknown");
     system("winget upgrade --all --include-unknown");
     printf("\nActualización completada.\n");
+    NewEntryFile();
     WaitKey();
     return;
     
@@ -352,6 +377,7 @@ void FlushDNS() {
     printf("Liberando la cache de DNS...\n");
     system("ipconfig /flushdns");
     printf("Cache de DNS liberada.\n");
+    NewEntryFile();
     WaitKey();
     return;
     
@@ -367,6 +393,7 @@ void CleanChrome() {
     system("rd /s /q \"%LOCALAPPDATA%\\Google\\Chrome\\User Data\\Default\\Service Worker\\CacheStorage\"");
     printf("LOCAL APP DATA >> Google Chrome >> CacheStorage [BORRADO]\n");
     printf("Cache de Google Chrome borrado.\n");
+    NewEntryFile();
     WaitKey();
     return;
 
@@ -377,6 +404,7 @@ void CleanStreamio() {
     printf("Limpiando la cache de Streamio...\n");
     system("rd /s /q \"C:\\Users\\ferna\\AppData\\Roaming\\stremio\\stremio-server\\stremio-cache\"");
     printf("Cache de Streamio limpio.\n");
+    NewEntryFile();
     WaitKey();
     return;
 
@@ -434,6 +462,7 @@ void WaitKey() {
 void RunCleanManager() {
 	
 	printf("Abriendo el 'Limpiador de Disco de Windows'...");
+	NewEntryFile();
 	system("cleanmgr.exe"); // Opens the Cleaner Manager
 	return;
 
@@ -443,13 +472,21 @@ void AboutLimpia() {
     
     ClearScreen();
     
-    printf("* Para que el programa funcione correctamente es necesario que se ejecute en modo\n");
-    printf("'Administrador de Sistema' para que pueda realizar todas las acciones. De lo contrario\n");
-    printf("Windows negara la mayoria de los comandos que intente aplicar el programa.\n\n");
+    printf("=============================================\n");
+    printf(" Limpia C 0.1 - Herramienta de mantenimiento \n");
+    printf("=============================================\n");
+    
+    printf("\n* Para que el programa funcione correctamente es necesario que se ejecute en modo\n");
+    printf(" 'Administrador de Sistema' para que pueda realizar todas las acciones. De lo contrario\n");
+    printf("  Windows negara la mayoria de los comandos que intente aplicar el programa.\n\n");
     
     printf("* La opcion 'Actualizar paquetes con winget' actualizara los programas instalados en Windows\n");
-    printf("a su version mas reciente. Si usted no desea actualizar o no sabe como esto puede afectar\n");
-    printf("el uso cotidiano de sus aplicaciones por favor evite utilizar esta opcion.\n\n");
+    printf("  a su version mas reciente. Si usted no desea actualizar o no sabe como esto puede afectar\n");
+    printf("  el uso cotidiano de sus aplicaciones por favor evite utilizar esta opcion.\n\n");
+    
+    printf("* Cierra todos los programas en uso antes de iniciar procesos de limpieza en este programa.\n");
+    printf("  Especialmente a la hora de limpiar archivos de Google Chrome y otra aplicaciones.\n");
+
 
     WaitKey();
     
@@ -470,7 +507,165 @@ void AboutLimpia() {
 	
 	printf("Limpieza automatica finalizada...\n");
 	SwitchAuto = 0;
-		
+	NewEntryFile();	
 	WaitKey();
 
 }
+
+//void SaveData() {
+	
+	//printf("Saving...");
+	
+	// Save the variable to a file
+	//fptr = fopen("DataFile.dat", "w+");
+	
+	//if (fptr == NULL) {
+		
+		//printf("Error opening file!\n");
+		//return;
+	//}
+
+    // Write variables to the file
+    //==================================================================
+    
+    // Config variables
+    //=================
+    
+    //fprintf(fptr, "%d\n", TimeDifferenceMODE);
+    
+    //printf("Config: Time Difference MODE [%d]\n", TimeDifferenceMODE);
+    
+    //Values and Prices
+    //=================
+    
+    //fprintf(fptr, "%f\n", SingleLessonPrice);
+    //fprintf(fptr, "%f\n", GroupLessonPrice);
+    //fprintf(fptr, "%f\n", NewSinglePriceBR);
+    //fprintf(fptr, "%f\n", NewGroupPriceBR);
+    //fprintf(fptr, "%f\n", DiscountValue);
+    //fprintf(fptr, "%f\n", ArgPrice);
+    //fprintf(fptr, "%f\n", USPrice);
+    
+    //printf("Single lesson price saved: %.2f\n", SingleLessonPrice);
+    //printf("Group lesson price saved: %.2f\n", GroupLessonPrice);
+	//printf("Single lesson price for new students saved: %.2f\n", NewSinglePriceBR);
+   // printf("Group lesson price for new students saved: %.2f\n", NewGroupPriceBR);
+	//printf("Discount value saved: %.2f\n", DiscountValue);
+	//printf("Argentina's price saved: %.2f\n", ArgPrice);
+	//printf("United States price saved: %.2f\n", USPrice);
+
+	    
+	//fclose(fptr);
+	
+	//}
+	
+void NewEntryFile() {
+	
+	if (SwitchAuto != 1) {
+		
+		printf("\nAgregando entrada en el archivo 'registro.txt'...");
+	
+		switch (ActionNumber) {
+			
+			case 1:
+			
+				strcpy (ActionText, "Actualizacion de paquetes con winget");
+				break;
+		
+			case 2:
+		
+				strcpy (ActionText, "Limpieza de archivos temporales");
+				break;
+			
+			case 3:
+		
+				strcpy (ActionText, "Carpeta temporal eliminada y recreada");
+				break;
+		
+			case 4:
+		
+				strcpy (ActionText, "Papelera de reciclaje vacia");
+				break;
+			
+			case 6:
+		
+				strcpy (ActionText, "Cache de DNS liberado");
+				break;
+			
+			case 7:
+		
+				strcpy (ActionText, "Cache de Google Chrome borrado");
+				break;
+			
+			case 8:
+		
+				strcpy (ActionText, "Cache de Streamio borrado");
+				break;
+				
+			case 9:
+		
+				strcpy (ActionText, "Limpiador de disco de Windows abierto");
+				break;
+			
+			case 12:
+		
+				strcpy (ActionText, "Limpieza automatica del sistema");
+				break;			
+			
+		}
+	
+		// Save the variable to a file
+		fptr = fopen("registro.txt", "a");
+	
+		if (fptr == NULL) {
+		
+			printf("Error abriendo archivo!\n");
+			return;
+		
+		}
+	
+		//==============================================================
+		// REQUESTING DATE AND TIME
+		
+		time_t now = time(NULL); 
+		struct tm *local = localtime(&now);
+
+		strftime(fecha, sizeof(fecha), "%d-%m-%Y", local);
+        
+		//==============================================================
+		// Write variables to the file
+
+		fprintf(fptr, "\n* %s | %20s", fecha, ActionText);
+		fclose(fptr);
+		
+	}
+		
+	return;
+	
+}
+
+Notas:
+
+//======================================================================
+//                               Notas 
+//======================================================================
+
+// Version 0.1
+
+//* Estructura general del programa creada.
+//* Actualización de paquetes con winget
+//* Limpiar archivos temporales.
+//* Eliminar y recrear la carpeta temporal.
+//* Vaciar la papelera de reciclaje.
+//* Mostrar servicios innecesarios.
+//* Liberar la cache de DNS.
+//* Limpiar cache de Google Chrome.
+//* Limpiar cache de Streamio.
+//*	Desactivar servicios no esenciales.
+//*	Abrir 'Limpiador de Disco' de Windows.
+//* Limpiar automaticamente el sistema.
+//*	Apartado agregado con anotaciones "Sobre Limpia".
+//* Las acciones del usuario quedan registradas en 'registro.txt' para
+//  su posterior consulta.
+
+//======================================================================
